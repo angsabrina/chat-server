4ec5b10a17c622e05d543bc08ea1108007b35e33
angsabrina <sabrina.ang@gatech.edu>
2018-12-29 21:53:11 +0800

    can change server address and port number of server and client

--- client.c
+++ client.c
@@ -9,14 +9,30 @@ int fd;
 int conn;
 char message[100] = "";
 
-int main() {
+int main(int argc, char *argv[]) {
+
+	if(argc < 2) {
+		fprintf(stderr, "ERROR, no address provided\n");
+		exit(1);
+	} else if(argc < 3) {
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+
+	int portNum = atoi(argv[2]);
+	// printf("server address: %s", argv[1]);
+	char serverAddr[20];
+	strcpy(serverAddr, argv[1]);
 
 	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
+		error("ERROR opening socket");
+	}
 
 	server.sin_family = AF_INET;
-	server.sin_port = htons(8096);
+	server.sin_port = htons(portNum);
 
-	inet_pton(AF_INET, "127.0.0.1", &server.sin_addr); //binds client to localhost
+	inet_pton(AF_INET, serverAddr, &server.sin_addr); //binds client to localhost
 	connect(fd, (struct sockaddr *)&server, sizeof(server)); //connects client to server
 
 	while(1) {
--- server.c
+++ server.c
@@ -23,26 +23,67 @@ int conn; //connection file descriptor, used to identify client connections
 char message[100] = ""; //array store msgs sent by server
 
 //hardcoded port
-int main() {
-	serv.sin_family = AF_INET;
-	serv.sin_port = htons(8096); //converts unsign short to network byte order, literally "host to network short"
-	serv.sin_addr.s_addr = INADDR_ANY; //binds the socket to all available interfaces
-	//server.sin_addr.s_addr = inet_addr("127.0.0.1") binds to localhost only
+// int main() {
+// 	serv.sin_family = AF_INET;
+// 	serv.sin_port = htons(8096); //converts unsign short to network byte order, literally "host to network short"
+// 	serv.sin_addr.s_addr = INADDR_ANY; //binds the socket to all available interfaces
+// 	//server.sin_addr.s_addr = inet_addr("127.0.0.1") binds to localhost only
 
-	fd = socket(AF_INET, SOCK_STREAM, 0); //creates new socket and returns identifier of socket into fd
-	if(fd < 0) {
+// 	fd = socket(AF_INET, SOCK_STREAM, 0); //creates new socket and returns identifier of socket into fd
+// 	if(fd < 0) {
+// 		error("ERROR opening socket");
+// 	}
+
+// 	bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //assigns addr specified by serv to socket
+// 	listen(fd, 5); //listen for client connections, max 5 allow
+
+// 	while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
+// 		int pid;
+// 		if((pid = fork()) == 0) {
+// 			while(recv(conn, message, 100, 0) > 0) {
+// 				printf("Message received: %s\n", message);
+// 				//extra breaking condition to terminate child process
+// 				memset(message, 0, sizeof(message));
+// 			}
+// 			exit(0);
+// 		}
+// 	}
+// 	return 0;
+// }
+
+
+//choose your own address and port
+int main(int argc, char *argv[]) {
+	// printf("arg nums: %i", argc);
+
+	if(argc < 2) {
+		fprintf(stderr, "ERROR, no address provided\n");
+		exit(1);
+	} else if(argc < 3) {
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+
+	int portNum = atoi(argv[2]);
+	//printf("port num: %i", portNum);
+
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd < 0) {
 		error("ERROR opening socket");
 	}
 
-	bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //assigns addr specified by serv to socket
-	listen(fd, 5); //listen for client connections, max 5 allow
+	serv.sin_family = AF_INET;
+	serv.sin_port = htons(portNum);
+	serv.sin_addr.s_addr = inet_addr(argv[1]);
+
+	bind(fd, (struct sockaddr *)&serv, sizeof(serv));
+	listen(fd, 5);
 
 	while(conn = accept(fd, (struct sockaddr *)NULL, NULL)) {
 		int pid;
 		if((pid = fork()) == 0) {
 			while(recv(conn, message, 100, 0) > 0) {
 				printf("Message received: %s\n", message);
-				//extra breaking condition to terminate child process
 				memset(message, 0, sizeof(message));
 			}
 			exit(0);
@@ -50,20 +91,5 @@ int main() {
 	}
 	return 0;
 }
-
-
-//choose your own port
-// int main(int argc, char *argv[]) {
-// 	if(argc < 2) {
-// 		fprintf(stderr, "ERROR, no port provided\n");
-// 		exit(1);
-// 	}
-
-// 	fd = socket(AF_INET, SOCK_STREAM, 0);
-// 	if (sockfd < 0) {
-// 		error("ERROR opening socket");
-// 	}
-
-// 	serv.sin_family = AF_INET;
 
 
